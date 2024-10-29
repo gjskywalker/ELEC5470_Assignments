0026bcc72c14168a22a6b8ad5c030e34f602ddbd
gskywalker <gj869@qq.com>
2024-10-29 14:39:13 +0800

    D1NewtonMethod

---
+++ Assignment_4/NewtonMethond.py
@@ -0,0 +1,16 @@
+from typing import Callable
+
+def D1NewtonMethod(f : Callable[[float], float], df: Callable[[float], float], d2f: Callable[[float], float], epsilon: float, stesize: float, init: float) -> float:
+    xk = init
+    while df(xk) * df(xk) / d2f(xk) > epsilon:
+        xk = xk - stesize * df(xk) / d2f(xk)
+    return xk
+
+
+if __name__ == "__main__":
+    f = lambda x: x**3 - x**2 - 1
+    df = lambda x: 3*x**2 - 2*x
+    d2f = lambda x: 6*x - 2
+    optimal_point = D1NewtonMethod(f, df, d2f, 1e-10, 1e-2, 1)
+    print(optimal_point)
+    
