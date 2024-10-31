511685b3389a6cf81913656b1137b68240edc64f
gskywalker <gj869@qq.com>
2024-10-31 14:20:47 +0800

    Finished SCA

--- Assignment_4/Problem1/SCA.py
+++ Assignment_4/Problem1/SCA.py
@@ -19,7 +19,7 @@ class SCA4LASSO:
     def update_variable(self, vn: np.ndarray, tau: float) -> np.ndarray:
         for i in range(0, self.p):
             y_t = self.y - self.bcd.sum_back(vn, i) - self.bcd.sum_font(vn, i)
-            vn[i] = self.bcd.soft(np.matmul(self.X[:,i].T, y_t) + tau*vn[i], self.lm) / (tau + np.linalg.norm(self.X[:,i], ord=2))
+            vn[i] = self.bcd.soft(np.matmul(self.X[:,i].T, y_t) + tau*vn[i], self.lm) / (tau + np.linalg.norm(self.X[:,i], ord=2)**2)
         return vn
     
     def run(self, tau:float, gamma: float) -> tuple:
@@ -28,15 +28,17 @@ class SCA4LASSO:
         optimal_value = 0
         cnt = 0
         print("Initial point: {0}, Initial value: {1}".format(vn, old_value))
-        while old_value != optimal_value:
+        while cnt < 10:
             old_value = optimal_value
             vn = vn + gamma * (self.update_variable(vn, tau) - vn)
             optimal_value = self.bcd.object_function(vn)
+            cnt += 1
+            print("Iteration: {0}, Current Point: {1}, Current Value: {2}".format(cnt, vn, optimal_value))
         return vn, self.bcd.object_function(vn)
     
 if __name__ == "__main__":
     sca = SCA4LASSO()
-    optimal_point, optimal_value = sca.run(tau=1, gamma=0.3)
+    optimal_point, optimal_value = sca.run(tau=0.1, gamma=0.3)
     print("Optimal_point: {0}, Optimal_value: {1}".format(optimal_point, optimal_value))
     
     
